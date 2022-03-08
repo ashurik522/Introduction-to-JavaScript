d44dd8fd6bb520dfe1db265e9bfca779ff30b5ef
ashurik522 <ashurik522@gmail.com>
2022-03-08 16:19:57 -0500

    complete task 6

--- index.js
+++ index.js
@@ -284,9 +284,13 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
-}
+function annoyingSong(startNum){
+  return `${startNum} bottles of soda on  the wall, ${startNum} bottles of soda, take one down pass it around ${startNum - 1} bottles of soda on the wall`
+};
+
+for (i = 2; i > 0; i--) {
+  console.log(annoyingSong(i));
+};
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
